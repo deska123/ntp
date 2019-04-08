f55c2954e4e60de258fe7897e6e1fb9ac5d22862
Dennis P Kamah <dkamah913@gmail.com>
2019-04-08 17:19:32 +0700

    Check captcha text

--- email.php
+++ email.php
@@ -2,6 +2,8 @@
 	use PHPMailer\PHPMailer\PHPMailer;
 	use PHPMailer\PHPMailer\Exception;
 
+	include 'securimage/securimage.php';
+
 	require 'PHPMailer-master/src/Exception.php';
 	require 'PHPMailer-master/src/PHPMailer.php';
 	require 'PHPMailer-master/src/SMTP.php';
@@ -28,120 +30,136 @@
 
 	if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest') {
 		if($_SERVER['REQUEST_METHOD'] === 'POST') {
-			$name = test_input($_POST['nameInput']);
-			$email = test_input($_POST['emailInput']);
-			$comments = test_input($_POST['commentsInput']);
+			if($_POST['command'] == 'checkingCaptcha') {
+				session_start();
 
-			// Instantiation and passing `true` enables exceptions
-			$mail = new PHPMailer(true);
-			$mail2 = new PHPMailer(true);
+				$captchaText = $_POST['captchaTextInput'];
+				$securimage = new Securimage();
 
-			try {
-			    //Server settings
-				$mail->SMTPOptions = array(
-				    'ssl' => array(
-				    'verify_peer' => false,
-				    'verify_peer_name' => false,
-				    'allow_self_signed' => true
-					)
-				);
-			    $mail->isSMTP();                                            // Set mailer to use SMTP
-			    $mail->Host       = '';  // Specify main and backup SMTP servers
-			    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-			    $mail->Username   = '';                     // SMTP username
-			    $mail->Password   = '';                               // SMTP password
-			    $mail->SMTPSecure = 'ssl';                                  // Enable TLS encryption, `ssl` also accepted
-			    $mail->Port       = 80;                                    // TCP port to connect to
-			    $mail2->SMTPOptions = array(
-				    'ssl' => array(
-				    'verify_peer' => false,
-				    'verify_peer_name' => false,
-				    'allow_self_signed' => true
-					)
-				);
-			    $mail2->isSMTP();                                            // Set mailer to use SMTP
-			    $mail2->Host       = '';  // Specify main and backup SMTP servers
-			    $mail2->SMTPAuth   = true;                                   // Enable SMTP authentication
-			    $mail2->Username   = '';                     // SMTP username
-			    $mail2->Password   = '';                               // SMTP password
-			    $mail2->SMTPSecure = 'ssl';                                  // Enable TLS encryption, `ssl` also accepted
-			    $mail2->Port       = 80;                                    // TCP port to connect to
+				if ($securimage->check($_POST['captchaTextInput']) == false) {
+					echo "not matched";
+				} else {
+					echo "matched";
+				}
+			} else if($_POST['command'] == 'sendingMessage') {
 
-			    //Recipients
-			    $mail->setFrom('', '');
-			    $mail->addAddress($email);     // Add a recipient
-			    $mail2->setFrom('', '');
-			    $mail2->addAddress('');     // Add a recipient
+				$name = test_input($_POST['nameInput']);
+				$email = test_input($_POST['emailInput']);
+				$comments = test_input($_POST['commentsInput']);
 
-			    // Content
-			    $mail->isHTML(true);                                  // Set email format to HTML
-			    $mail->Subject = 'Your Comments on NTP Website';
-			    $mail2->isHTML(true);                                  // Set email format to HTML
-			    $mail2->Subject = 'Comments sent on ' . $dateTimeNow;
+				// Instantiation and passing `true` enables exceptions
+				$mail = new PHPMailer(true);
+				$mail2 = new PHPMailer(true);
 
-  				$confirmEmailBody = "
-	  				<h2>Here is detail of your comment sent to NTP Website</h2>
-					<img src=\"\" alt=\"Email Image\" style=\"width:100%\">
-					<h2>Nariska Tresnandy Pratama</h2>
-					<ul>
-						<li>
-							<p><u>Your Name</u></p>
-							<p>" . $name . "</p>
-						</li>
-						<li>
-							<p><u>Your Email</u></p>
-							<p>" . $email . "</p>
-						</li>
-						<li>
-							<p><u>Your IP Address</u></p>
-							<p>" . $ipaddress . "</p>
-						</li>
-						<li>
-							<p><u>Your Comment</u></p>
-							<p>" . $comments . "</p>
-						</li>
-					</ul>
-					<div>Sent on " . $dateTimeNow . "</div>
-  				";
+				try {
+				    //Server settings
+					$mail->SMTPOptions = array(
+					    'ssl' => array(
+					    'verify_peer' => false,
+					    'verify_peer_name' => false,
+					    'allow_self_signed' => true
+						)
+					);
+				    $mail->isSMTP();                                            // Set mailer to use SMTP
+				    $mail->Host       = '';  // Specify main and backup SMTP servers
+				    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+				    $mail->Username   = '';                     // SMTP username
+				    $mail->Password   = '';                               // SMTP password
+				    $mail->SMTPSecure = 'ssl';                                  // Enable TLS encryption, `ssl` also accepted
+				    $mail->Port       = 80;                                    // TCP port to connect to
+				    $mail2->SMTPOptions = array(
+					    'ssl' => array(
+					    'verify_peer' => false,
+					    'verify_peer_name' => false,
+					    'allow_self_signed' => true
+						)
+					);
+				    $mail2->isSMTP();                                            // Set mailer to use SMTP
+				    $mail2->Host       = '';  // Specify main and backup SMTP servers
+				    $mail2->SMTPAuth   = true;                                   // Enable SMTP authentication
+				    $mail2->Username   = '';                     // SMTP username
+				    $mail2->Password   = '';                               // SMTP password
+				    $mail2->SMTPSecure = 'ssl';                                  // Enable TLS encryption, `ssl` also accepted
+				    $mail2->Port       = 80;                                    // TCP port to connect to
 
-  				$sentEmailBody = "
-	  				<h2>Here is detail of comment sent to NTP Website</h2>
-					<img src=\"\" alt=\"Email Image\" style=\"width:100%\">
-					<h2>Nariska Tresnandy Pratama</h2>
-					<ul>
-						<li>
-							<p><u>Name</u></p>
-							<p>" . $name . "</p>
-						</li>
-						<li>
-							<p><u>Email</u></p>
-							<p>" . $email . "</p>
-						</li>
-						<li>
-							<p><u>IP Address</u></p>
-							<p>" . $ipaddress . "</p>
-						</li>
-						<li>
-							<p><u>Comment</u></p>
-							<p>" . $comments . "</p>
-						</li>
-					</ul>
-					<div>Sent on " . $dateTimeNow . "</div>
-  				";
+				    //Recipients
+				    $mail->setFrom('', '');
+				    $mail->addAddress($email);     // Add a recipient
+				    $mail2->setFrom('', '');
+				    $mail2->addAddress('');     // Add a recipient
 
-			    $mail->Body    = $confirmEmailBody;
-			    $mail2->Body    = $sentEmailBody;
+				    // Content
+				    $mail->isHTML(true);                                  // Set email format to HTML
+				    $mail->Subject = 'Your Comments on NTP Website';
+				    $mail2->isHTML(true);                                  // Set email format to HTML
+				    $mail2->Subject = 'Comments sent on ' . $dateTimeNow;
 
-			    $mail->AltBody = 'Here is detail of your comment : [Name] ' . $name . ' ; [Email] ' . $email . ' ; [IP Address] ' . $ipaddress . ' ; [Comments] ' . $comments;
-			    $mail2->AltBody = 'Here is detail of comment sent to NTP Website : [Name] ' . $name . ' ; [Email] ' . $email . ' ; [IP Address] ' . $ipaddress . ' ; [Comments] ' . $comments;
+	  				$confirmEmailBody = "
+		  				<h2>Here is detail of your comment sent to NTP Website</h2>
+						<img src=\"https://nariska.000webhostapp.com/ntp/assets/images/email.png\" alt=\"Email Image\" style=\"width:100%\">
+						<h2>Nariska Tresnandy Pratama</h2>
+						<ul>
+							<li>
+								<p><u>Your Name</u></p>
+								<p>" . $name . "</p>
+							</li>
+							<li>
+								<p><u>Your Email</u></p>
+								<p>" . $email . "</p>
+							</li>
+							<li>
+								<p><u>Your IP Address</u></p>
+								<p>" . $ipaddress . "</p>
+							</li>
+							<li>
+								<p><u>Your Comment</u></p>
+								<p>" . $comments . "</p>
+							</li>
+						</ul>
+						<div>Sent on " . $dateTimeNow . "</div>
+	  				";
 
-			    $mail->send();
-			    $mail2->send();
+	  				$sentEmailBody = "
+		  				<h2>Here is detail of comment sent to NTP Website</h2>
+						<img src=\"https://nariska.000webhostapp.com/ntp/assets/images/email.png\" alt=\"Email Image\" style=\"width:100%\">
+						<h2>Nariska Tresnandy Pratama</h2>
+						<ul>
+							<li>
+								<p><u>Name</u></p>
+								<p>" . $name . "</p>
+							</li>
+							<li>
+								<p><u>Email</u></p>
+								<p>" . $email . "</p>
+							</li>
+							<li>
+								<p><u>IP Address</u></p>
+								<p>" . $ipaddress . "</p>
+							</li>
+							<li>
+								<p><u>Comment</u></p>
+								<p>" . $comments . "</p>
+							</li>
+						</ul>
+						<div>Sent on " . $dateTimeNow . "</div>
+	  				";
 
-			    echo 'Success';
-			} catch (Exception $e) {
-			    echo "Failed";
+				    $mail->Body    = $confirmEmailBody;
+				    $mail2->Body    = $sentEmailBody;
+
+				    $mail->AltBody = 'Here is detail of your comment : [Name] ' . $name . ' ; [Email] ' . $email . ' ; [IP Address] ' . $ipaddress . ' ; [Comments] ' . $comments;
+				    $mail2->AltBody = 'Here is detail of comment sent to NTP Website : [Name] ' . $name . ' ; [Email] ' . $email . ' ; [IP Address] ' . $ipaddress . ' ; [Comments] ' . $comments;
+
+				    $mail->send();
+				    $mail2->send();
+
+				    echo 'Success';
+				} catch (Exception $e) {
+				    echo "Failed";
+				}
 			}
+		} else if($_SERVER['REQUEST_METHOD'] === 'GET') {
+			session_start();
 		}
 	} else {
 		header("Location: index.html");
